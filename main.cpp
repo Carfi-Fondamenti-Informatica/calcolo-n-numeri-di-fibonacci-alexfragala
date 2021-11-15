5a08a749145290ed4916636235cf2e7fcff5949c
alexfragala <91506326+alexfragala@users.noreply.github.com>
2021-11-15 04:27:39 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,25 +2,21 @@
 using namespace std;
 
 int main() {
-    int a = 0, b = 1, c = 0, n = 0;
+    int a = 1, b = 0, i = 0, n = 0, c = 0;
     cin >> n;
 
-    if(n<2){
+    if (n < 2) {
         cout << "errore" << endl;
-
-    }else if(n>=2) {
-        cout << "1" << endl;
-
-        for (int i = 2; i > 0; i++) {
+    } else {
+        
+        for (i = 0; i <=(n-1); i++)
+            {
             c = a + b;
-            if (c <= n) {
-                cout << c << endl;
-            } else {
-                return 0;
-            }
+            cout << c << endl;
             a = b;
             b = c;
+
         }
+        return 0;
     }
-    return 0;
 }
